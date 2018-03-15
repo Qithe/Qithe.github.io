c4928a9b5e17e66e97a037dac9d03cb2d1b9bcf8
Samuel Gustafsson <SamuelGustafsson@hotmail.com>
2018-03-15 19:26:14 +0100

    Added some functions

---
+++ Labbar/Script.js
@@ -0,0 +1,71 @@
+// Your code here!
+let url = 'https://www.forverkliga.se/JavaScript/api/crud.php';
+let theKey;
+
+
+function GetApiKey() {
+    let querystring = '?requestKey';
+    let newUrl = url+querystring;
+
+    // Gör en AJAX request och visa resultatet
+    let ajax = new XMLHttpRequest();
+    ajax.open('get', newUrl);
+    ajax.onreadystatechange = function () {
+        if (ajax.status === 200 && ajax.readyState === 4) {
+            // AJAX lyckades, presentera svaret
+            let json = JSON.parse(ajax.responseText);
+            console.log(json);
+            theKey = json.key;
+            document.getElementById('OutputMessage').innerHTML = 'Din key är ' + theKey + ', bara så du vet ;)';
+        }
+        else if (ajax.status !== 200) {
+            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
+        }
+    };
+    ajax.send();
+}
+
+function AddData() {
+    let querystring = '?op=insert&key=' + theKey + '&title=Första Boken&author=Mrs Andersson';
+    let newUrl = url+querystring;
+
+    // Gör en AJAX request och visa resultatet
+    let ajax = new XMLHttpRequest();
+    ajax.open('get', newUrl);
+    ajax.onreadystatechange = function () {
+        if (ajax.status === 200 && ajax.readyState === 4) {
+            // AJAX lyckades, presentera svaret
+            let json = JSON.parse(ajax.responseText);
+            console.log(json);
+            document.getElementById('OutputMessage').innerHTML += json.status;
+            //document.getElementById('Booklist').innerHTML = 'Success';
+        }
+        else if (ajax.status !== 200) {
+            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
+        }
+    };
+    ajax.send();
+    ViewData();
+}
+
+function ViewData() {
+    let querystring = '?op=select&key=' + theKey;
+    let newUrl = url+querystring;
+    
+    let ajax = new XMLHttpRequest();
+    ajax.open('get', newUrl);
+    ajax.onreadystatechange = function () {
+        if (ajax.status === 200 && ajax.readyState === 4) {
+            // AJAX lyckades, presentera svaret
+            let json = JSON.parse(ajax.responseText);
+            console.log(json);
+            document.getElementById('OutputMessage').innerHTML += json.title;
+            document.getElementById('OutputMessage').innerHTML += json.author;
+            //document.getElementById('Booklist').innerHTML = 'Success';
+        }
+        else if (ajax.status !== 200) {
+            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
+        }
+    };
+    ajax.send();
+}
