d1078f7fd2f74a6707236a290add4e6c2ea2b4bc
Oscar Wolter <Oscardawolter@gmail.com>
2018-03-20 15:36:12 +0100

    Added stuff!
    Ändrat massa skit

--- Labbar/Script.js
+++ Labbar/Script.js
@@ -2,6 +2,13 @@
 let url = 'https://www.forverkliga.se/JavaScript/api/crud.php';
 let theKey;
 
+function wait(ms) {
+    var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
 
 function GetApiKey() {
     let querystring = '?requestKey';
@@ -26,9 +33,38 @@ function GetApiKey() {
 }
 
 function AddData() {
-    let querystring = '?op=insert&key=' + theKey + '&title=Första Boken&author=Mrs Andersson';
+    let title = document.getElementById('Title').value;
+    let author = document.getElementById('Author').value;
+    var PushSuccessful = false;
+    var tries = 0;
+    
+    if(title!=='' && author!==''){
+        while(!PushSuccessful || tries>=9){
+            var statusMessage = PushToDatabase(title,author); 
+            if (statusMessage == 'success'){
+                PushSuccessful = true;
+            } else {
+                tries++;
+                console.log('Try nr'+tries+1);
+                if(tries>=9){
+                    console.log('Push unsuccsssesful')
+                    document.getElementById('OutputMessage').innerHTML = 'Något gick fel, var god görsök igen';
+                }
+            }
+        }
+        document.getElementById('Title').value='';
+        document.getElementById('Author').value='';
+    }
+    else{
+        document.getElementById('OutputMessage').innerHTML = 'Du måste fylla i både en titel och författare';
+        console.log('Författare och/eller titel saknades i sina text fält');
+    }
+}
+ 
+function PushToDatabase(title, author){
+    var statusMessage = '';
+    let querystring = '?op=insert&key=' + theKey + '&title='+title+'&author='+author;
     let newUrl = url+querystring;
-
     // Gör en AJAX request och visa resultatet
     let ajax = new XMLHttpRequest();
     ajax.open('post', newUrl);
@@ -36,7 +72,8 @@ function AddData() {
         if (ajax.status === 200 && ajax.readyState === 4) {
             // AJAX lyckades, presentera svaret
             let json = JSON.parse(ajax.responseText);
-            console.log('AddData ' + json);
+            console.log('AddData was: ' + json.status);
+            statusMessage = json.status;
             document.getElementById('OutputMessage').innerHTML += ' ' + json.status;
             //document.getElementById('Booklist').innerHTML = 'Success';
         }
@@ -45,23 +82,29 @@ function AddData() {
         }
     };
     ajax.send();
-    ViewData();
+    return statusMessage;
 }
 
 function ViewData() {
     let querystring = '?op=select&key=' + theKey;
     let newUrl = url+querystring;
+    var x = "",i;
     
     let ajax = new XMLHttpRequest();
     ajax.open('get', newUrl);
     ajax.onreadystatechange = function () {
         if (ajax.status === 200 && ajax.readyState === 4) {
             // AJAX lyckades, presentera svaret
-            let json = JSON.parse(ajax.responseText);
-            console.log('ViewData ' + json[0].Object);
-            document.getElementById('BooksAuthors').value = 'HejHej';
-            document.getElementById('OutputMessage').innerHTML += ' ' + json;
-            //document.getElementById('Booklist').innerHTML = 'Success';
+            //Tar JSON(responsetext) och gör den till json(json)
+            let json = JSON.parse(ajax.responseText); 
+            //Ta ut arrayen ur json
+            for (i in json.data) {
+                x += "<option>"+ json.data[i].title + ", " + json.data[i].author +"</option>";
+                console.log(x);
+            }
+            document.getElementById('BooksAuthors').innerHTML += x;
+            
+            
         }
         else if (ajax.status !== 200) {
             document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
