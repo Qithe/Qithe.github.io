830289d033ad0cf060bd7f392736a007a37cafe9
Samuel Gustafsson <SamuelGustafsson@hotmail.com>
2018-03-22 11:45:54 +0100

    Fix efter merge

--- Labbar/Script.js
+++ Labbar/Script.js
@@ -2,6 +2,13 @@
 let url = 'https://www.forverkliga.se/JavaScript/api/crud.php';
 let theKey;
 
+function wait(ms) {
+    var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
 
 function GetApiKey() {
     let querystring = '?requestKey';
@@ -25,9 +32,38 @@ function GetApiKey() {
 }
 
 function AddData() {
-    let querystring = '?op=insert&key=' + theKey + '&title=Första Boken&author=Mrs Andersson';
+    let title = document.getElementById('Title').value;
+    let author = document.getElementById('Author').value;
+    var PushSuccessful = false;
+    var tries = 0;
+    
+    if(title!=='' && author!==''){
+        while(!PushSuccessful || tries>=9){
+            var statusMessage = PushToDatabase(title,author); 
+            if (statusMessage == 'success'){
+                PushSuccessful = true;
+            } else {
+                tries++;
+                console.log('Try nr'+tries+1);
+                if(tries>=9){
+                    console.log('Push unsuccsssesful')
+                    document.getElementById('OutputMessage').innerHTML = 'Något gick fel, var god görsök igen';
+                }
+            }
+        }
+        document.getElementById('Title').value='';
+        document.getElementById('Author').value='';
+    }
+    else{
+        document.getElementById('OutputMessage').innerHTML = 'Du måste fylla i både en titel och författare';
+        console.log('Författare och/eller titel saknades i sina text fält');
+    }
+}
+ 
+function PushToDatabase(title, author){
+    var statusMessage = '';
+    let querystring = '?op=insert&key=' + theKey + '&title='+title+'&author='+author;
     let newUrl = url+querystring;
-
     // Gör en AJAX request och visa resultatet
     fetch(newUrl)
     .then(function(response){
@@ -45,7 +81,6 @@ function AddData() {
     .catch(function(message){
         document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request i AddData.';
     })
-    //ViewData();
 }
 
 function ViewData() {
