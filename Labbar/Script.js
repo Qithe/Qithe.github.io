f7a904da448336ea4334cf66849172390c78ebbf
Samuel Gustafsson <SamuelGustafsson@hotmail.com>
2018-03-22 11:21:47 +0100

    Changed AJAX usage to fetch
    Changed the AJAX request to fetch(url)

--- Labbar/Script.js
+++ Labbar/Script.js
@@ -8,21 +8,20 @@ function GetApiKey() {
     let newUrl = url+querystring;
 
     // Gör en AJAX request och visa resultatet
-    let ajax = new XMLHttpRequest();
-    ajax.open('get', newUrl);
-    ajax.onreadystatechange = function () {
-        if (ajax.status === 200 && ajax.readyState === 4) {
-            // AJAX lyckades, presentera svaret
-            let json = JSON.parse(ajax.responseText);
-            console.log(json);
-            theKey = json.key;
-            document.getElementById('OutputMessage').innerHTML = 'Din key är ' + theKey + ', bara så du vet ;)';
-        }
-        else if (ajax.status !== 200) {
-            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
-        }
-    };
-    ajax.send();
+    //let ajax = new XMLHttpRequest();
+    fetch(newUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        // AJAX lyckades, presentera svaret
+        console.log(data);
+        theKey = data.key;
+        document.getElementById('OutputMessage').innerHTML = 'Din key är ' + theKey + ', bara så du vet ;)';
+    })
+    .catch(function(message){
+        document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request i GetApiKey.';
+    });
 }
 
 function AddData() {
@@ -30,42 +29,45 @@ function AddData() {
     let newUrl = url+querystring;
 
     // Gör en AJAX request och visa resultatet
-    let ajax = new XMLHttpRequest();
-    ajax.open('post', newUrl);
-    ajax.onreadystatechange = function () {
-        if (ajax.status === 200 && ajax.readyState === 4) {
-            // AJAX lyckades, presentera svaret
-            let json = JSON.parse(ajax.responseText);
-            console.log('AddData ' + json);
-            document.getElementById('OutputMessage').innerHTML += ' ' + json.status;
-            //document.getElementById('Booklist').innerHTML = 'Success';
+    fetch(newUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('OutputMessage').innerHTML += 'AD ' + data.status;
+    })
+    /*.then(function(data){
+        if(data.status === 'Success'){
+            ViewData();
         }
-        else if (ajax.status !== 200) {
-            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
-        }
-    };
-    ajax.send();
-    ViewData();
+    })*/
+    .catch(function(message){
+        document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request i AddData.';
+    })
+    //ViewData();
 }
 
 function ViewData() {
     let querystring = '?op=select&key=' + theKey;
     let newUrl = url+querystring;
+    let previousContent = document.getElementById('BooksAuthors').innerHTML;
     
-    let ajax = new XMLHttpRequest();
-    ajax.open('get', newUrl);
-    ajax.onreadystatechange = function () {
-        if (ajax.status === 200 && ajax.readyState === 4) {
-            // AJAX lyckades, presentera svaret
-            let json = JSON.parse(ajax.responseText);
-            console.log('ViewData ' + json[0].Object);
-            document.getElementById('BooksAuthors').value = 'HejHej';
-            document.getElementById('OutputMessage').innerHTML += ' ' + json;
-            //document.getElementById('Booklist').innerHTML = 'Success';
-        }
-        else if (ajax.status !== 200) {
-            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request.';
-        }
-    };
-    ajax.send();
+    //do{
+        fetch(newUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data.data);
+            document.getElementById('BooksAuthors').innerHTML = '';
+            for(i in data.data){
+                console.log(data.data[i].title);
+                document.getElementById('BooksAuthors').innerHTML += '<option value="book">' + data.data[i].title + ', ' + data.data[i].author;
+            }    
+        })
+        .catch(function(message){
+            document.getElementById('OutputMessage').innerHTML = 'Ett okänt fel inträffade vid AJAX request i ViewData.';
+        })
+    //}while(document.getElementById('BooksAuthors').innerHTML === previousContent);
 }
